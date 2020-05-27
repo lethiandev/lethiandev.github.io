e8e7adc5aa6a9386465150ccdc705cac3e715b4e
Konrad Nowakowski <konrad.x92@gmail.com>
2020-05-27 15:02:08 +0100

    Add popstate console logging

--- _src/index.ts
+++ _src/index.ts
@@ -1,3 +1,17 @@
 import { sayHello } from './hello';
 
-sayHello()
+window.addEventListener('popstate', ev => {
+  console.log('onpopstate', document.location, JSON.stringify(ev.state))
+  ev.preventDefault()
+})
+
+const hrefs = document.getElementsByTagName('a')
+for (let i = 0; i < hrefs.length; i++) {
+  const href = hrefs.item(i) as HTMLAnchorElement
+
+  href.addEventListener('click', ev => {
+    console.log('push state', href.href)
+    history.pushState({}, 'Project', href.href)
+    ev.preventDefault()
+  })
+}
--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -7,8 +7,23 @@ define("hello", ["require", "exports"], function (require, exports) {
     }
     exports.sayHello = sayHello;
 });
-define("index", ["require", "exports", "hello"], function (require, exports, hello_1) {
+define("index", ["require", "exports"], function (require, exports) {
     "use strict";
     exports.__esModule = true;
-    hello_1.sayHello();
+    window.addEventListener('popstate', function (ev) {
+        console.log('onpopstate', document.location, JSON.stringify(ev.state));
+        ev.preventDefault();
+    });
+    var hrefs = document.getElementsByTagName('a');
+    var _loop_1 = function (i) {
+        var href = hrefs.item(i);
+        href.addEventListener('click', function (ev) {
+            console.log('push state', href.href);
+            history.pushState({}, 'Project', href.href);
+            ev.preventDefault();
+        });
+    };
+    for (var i = 0; i < hrefs.length; i++) {
+        _loop_1(i);
+    }
 });
