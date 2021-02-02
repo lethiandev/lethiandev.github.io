007e9a6e1a32625101d0a8f2dfa3d4ecdbfdf828
Konrad Nowakowski <konrad.x92@gmail.com>
2021-02-02 17:55:56 +0000

    Add temporary console logging

--- _src/index.ts
+++ _src/index.ts
@@ -2,4 +2,9 @@ import { createTerminal } from './terminal'
 import { forEachElement } from './utils'
 
 const terminalElements = document.getElementsByClassName('terminal')
-forEachElement(terminalElements, createTerminal)
+forEachElement(terminalElements, initTerminal)
+
+async function initTerminal(el: HTMLElement) {
+  await createTerminal(el)
+  console.log("Terminal done!")
+}
