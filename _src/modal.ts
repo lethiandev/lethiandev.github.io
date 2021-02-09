eac6e221efc7a9e2d290e2adb8443a49fb454a87
Konrad Nowakowski <konrad.x92@gmail.com>
2021-02-09 19:51:40 +0000

    Fix gallery images max height in modals

--- _src/modal.ts
+++ _src/modal.ts
@@ -9,4 +9,35 @@ export function createModal(modal: HTMLElement) {
       }
     })
   }
+
+  // Also handle modal galleries
+  createModalGallery(modal)
+}
+
+function createModalGallery(modal: HTMLElement) {
+  const gallery = modal.getElementsByClassName('gallery').item(0)
+  const toggler = modal.previousElementSibling
+
+  if (gallery instanceof HTMLElement && toggler instanceof HTMLInputElement) {
+    toggler.addEventListener('change', ev => {
+      if (toggler.checked) {
+        updateGalleryImages(gallery)
+      }
+    })
+  }
+}
+
+function updateGalleryImages(gallery: HTMLElement) {
+  const images = gallery.getElementsByClassName('gallery-image')
+
+  // Calculate max content height
+  // TODO: Add paddings calculation based on elements
+  const height = window.innerHeight - 80
+
+  for(const image of Array.from(images)) {
+    const imgElement = image.children.item(0)
+    if (imgElement instanceof HTMLImageElement) {
+      imgElement.style.maxHeight = `${height}px`
+    }
+  }
 }
